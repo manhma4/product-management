c51d12f4301267d164617f503518140f429407f1
minhvu <manhminha4@gmail.com>
2024-11-04 22:20:26 +0700

    toi uu pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helper/filterStatus");
 const searchHelper = require("../../helper/search");
+const paginationHelper = require("../../helper/pagination");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -21,21 +22,13 @@ module.exports.index = async (req, res) => {
   }
 
   //Pagination
-  let objectPagination = {
-    currentPage: 1,
-    limitItems: 4,
-  };
-
-  if (req.query.page) {
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-
-  objectPagination.skip =
-    (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
   const countProducts = await Product.countDocuments(find);
-  const totalPage = Math.ceil(countProducts / objectPagination.limitItems);
-  objectPagination.totalPage = totalPage;
+
+  let objectPagination = paginationHelper(
+    { currentPage: 1, limitItems: 4 },
+    req.query,
+    countProducts
+  );
 
   //End Pagination
 
