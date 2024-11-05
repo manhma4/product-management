60b5d5bd1dfbb76cc3748ff84e1a76ad23c368e7
minhvu <manhminha4@gmail.com>
2024-11-05 21:31:45 +0700

    xoa nhieu san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -66,6 +66,15 @@ module.exports.changeMulti = async (req, res) => {
     case "inactive":
       await Product.updateMany({ _id: { $in: ids } }, { status: "inactive" });
       break;
+    case "delete-all":
+      await Product.updateMany(
+        { _id: { $in: ids } },
+        {
+          deleted: true,
+          deletedAt: new Date(),
+        }
+      );
+      break;
 
     default:
       break;
