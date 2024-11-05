bdc5e05c48737f945d3c291fd13c0f252f3ca607
minhvu <manhminha4@gmail.com>
2024-11-05 21:08:33 +0700

    xoa mem va them thoi gian xoa

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -77,6 +77,13 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
 
-  await Product.deleteOne({ _id: id });
+  // await Product.deleteOne({ _id: id });
+  await Product.updateOne(
+    { _id: id },
+    {
+      deleted: true,
+      deletedAt: new Date(),
+    }
+  );
   res.redirect("back");
 };
