54968205d3ed70099bd13cb6e6aba52a7828ab9a
minhvu <manhminha4@gmail.com>
2024-11-05 22:35:48 +0700

    tinh nang thay doi vi tri san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -33,6 +33,7 @@ module.exports.index = async (req, res) => {
   //End Pagination
 
   const products = await Product.find(find)
+    .sort({ position: "desc" })
     .limit(objectPagination.limitItems)
     .skip(objectPagination.skip);
 
@@ -74,6 +75,14 @@ module.exports.changeMulti = async (req, res) => {
           deletedAt: new Date(),
         }
       );
+      break;
+    case "change-position":
+      for (const item of ids) {
+        let [id, position] = item.split("-");
+        position = parseInt(position);
+
+        await Product.updateOne({ _id: id }, { position: position });
+      }
       break;
 
     default:
