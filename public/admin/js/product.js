67aadd4794e0604f993b717aa3df17a168414553
minhvu <manhminha4@gmail.com>
2024-11-05 20:50:32 +0700

    xoa vinh vien (xoa mem)

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -19,3 +19,29 @@ if (buttonsChangeStatus.length > 0) {
 }
 
 //End Change Status
+
+//Delete Item
+
+const buttonsDelete = document.querySelectorAll("[button-delete]");
+
+if (buttonsDelete.length > 0) {
+  const formDeleteItem = document.querySelector("#form-delete-item");
+  const path = formDeleteItem.getAttribute("data-path");
+
+  buttonsDelete.forEach((button) => {
+    button.addEventListener("click", () => {
+      const isConfirm = confirm("bạn có chắn muốn xóa sản phẩm này không ?");
+
+      if (isConfirm) {
+        const id = button.getAttribute("data-id");
+        const action = `${path}/${id}?_method=DELETE`;
+
+        formDeleteItem.action = action;
+
+        formDeleteItem.submit();
+      }
+    });
+  });
+}
+
+//End Delete Item
