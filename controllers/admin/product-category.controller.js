1f4a2dd4130b335f99927711a2cad92be3c8454e
minhvu <manhminha4@gmail.com>
2024-11-16 10:26:45 +0700

    phân câp cho phân danh muc

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -2,6 +2,7 @@ const systemConfig = require("../../config/system");
 const ProductCategory = require("../../models/product-category.model");
 const filterStatusHelper = require("../../helper/filterStatus");
 
+// [GET] /admin/products-category
 module.exports.index = async (req, res) => {
   // console.log(req.query)
   let find = {
@@ -17,22 +18,65 @@ module.exports.index = async (req, res) => {
     find.title = regex;
   }
 
+  function createTree(arr, parentId = "") {
+    const tree = [];
+    arr.forEach((item) => {
+      if (item.parent_id === parentId) {
+        const newItem = item;
+        const children = createTree(arr, item.id);
+        if (children.length > 0) {
+          newItem.children = children;
+        }
+        tree.push(newItem);
+      }
+    });
+    return tree;
+  }
+
   const records = await ProductCategory.find(find);
+
+  const treeRecords = createTree(records);
+
   res.render("admin/pages/product-category/index", {
     pageTitle: "Danh mục sản phẩm",
-    records: records,
+    records: treeRecords,
     filterStatus: filterStatus,
   });
 };
 
-module.exports.create = (req, res) => {
+// [GET] /admin/products-category/create
+module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  function createTree(arr, parentId = "") {
+    const tree = [];
+    arr.forEach((item) => {
+      if (item.parent_id === parentId) {
+        const newItem = item;
+        const children = createTree(arr, item.id);
+        if (children.length > 0) {
+          newItem.children = children;
+        }
+        tree.push(newItem);
+      }
+    });
+    return tree;
+  }
+
+  const records = await ProductCategory.find(find);
+  const treeRecords = createTree(records);
+
   res.render("admin/pages/product-category/create", {
     pageTitle: "Tạo danh mục sản phẩm",
+    records: treeRecords,
   });
 };
 
+// [POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   if (req.body.position) {
     req.body.position = parseInt(req.body.position);
   } else {
