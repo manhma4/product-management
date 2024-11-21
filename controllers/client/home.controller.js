2fb5df5da8395c38f1b5170b43f8585fd576fd03
minhvu <manhminha4@gmail.com>
2024-11-21 16:45:49 +0700

    Thêm sản phẩm mới bên trang client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -3,15 +3,28 @@ const productHelper = require("../../helper/product");
 
 // [GET] /
 module.exports.index = async (req, res) => {
-  const featuredProducts = await Product.find({
+  // Lấy sản phẩm nổi bật
+  const productFeatured = await Product.find({
     featured: "1",
     deleted: false,
     status: "active",
-  });
-  const newProducts = productHelper.getNewPrice(featuredProducts);
+  }).limit(6);
+  const newProductsFeatured = productHelper.getNewPrice(productFeatured);
+  //End lấy sản phẩm nổi bật
+
+  //Lấy sản phẩm mới nhất
+  const productNew = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+  const newProductNew = productHelper.getNewPrice(productNew);
+  //End lấy sản phẩm mới nhất
 
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    featuredProducts: newProducts,
+    productFeatured: newProductsFeatured,
+    productNew: newProductNew,
   });
 };
