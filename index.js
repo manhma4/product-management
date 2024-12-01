ff21f114efdf58882b9a4963b5991f304208e385
minhvu <manhminha4@gmail.com>
2024-12-01 17:33:13 +0700

    Làm trang 404 not found

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ app.use(express.static(`${__dirname}/public`));
 route(app);
 routeAdmin(app);
 
+app.get("*", (req, res) => {
+  res.render("client/pages/error/404", {
+    pageTitle: "404 Not Found",
+  });
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
