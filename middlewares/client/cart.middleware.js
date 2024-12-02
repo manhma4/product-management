1922338df3d5ea457b0346412d5957b4a3d3099c
minhvu <manhminha4@gmail.com>
2024-12-02 12:58:03 +0700

    chat realtime + sửa css

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -3,7 +3,7 @@ const Cart = require("../../models/cart.model");
 module.exports.cartId = async (req, res, next) => {
   if (!req.cookies.cartId) {
     const cart = new Cart();
-    // await cart.save();
+    await cart.save();
 
     const expiresTime = 1000 * 60 * 60 * 24 * 365;
     res.cookie("cartId", cart.id, {
@@ -14,12 +14,13 @@ module.exports.cartId = async (req, res, next) => {
     const cart = await Cart.findOne({
       _id: req.cookies.cartId,
     });
-
-    cart.totalQuantity = cart.products.reduce(
-      (sum, item) => sum + item.quantity,
-      0
-    );
-    res.locals.miniCart = cart;
+    if (cart) {
+      cart.totalQuantity = cart.products.reduce(
+        (sum, item) => sum + item.quantity,
+        0
+      );
+      res.locals.miniCart = cart;
+    }
   }
   next();
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -16,3 +16,38 @@ if (formSendData) {
 }
 
 // End CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  const chatForm = document.querySelector("[my-id]");
+  const myId = chatForm.getAttribute("my-id");
+
+  const body = document.querySelector(".chat .inner-body");
+  // console.log(body)
+  const div = document.createElement("div");
+  let htmlFullName = "";
+  if (myId == data.userId) {
+    div.classList.add("inner-outgoing");
+  } else {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+  }
+  div.innerHTML = `
+        ${htmlFullName}
+        <div class="inner-content">${data.content}</div>
+    `;
+  body.appendChild(div);
+});
+// End SERVER_RETURN_MESSAGE
+
+// Function to scroll the chat to the bottom
+function scrollToBottom() {
+  const chatBody = document.querySelector(".inner-body");
+  chatBody.scrollTop = chatBody.scrollHeight;
+}
+// Scroll to bottom when the page loads
+scrollToBottom();
+// Example: Call scrollToBottom function whenever a new message is added
+const chatContainer = document.querySelector(".inner-body");
+const observer = new MutationObserver(scrollToBottom);
+observer.observe(chatContainer, { childList: true });
